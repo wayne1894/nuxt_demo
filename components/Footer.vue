2304e439f9ed5e794e1125ea1648d10efc1a21aa
wayne1894 <mose286778@gmail.com>
2020-07-28 06:18:32 +0800

    final

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,7 +2,7 @@
 <footer id="footer">
 						<div class="inner">
 							<section>
-								<h2>Get in touch</h2>
+								<h2>與我聯絡</h2>
 								<form method="post" action="#">
 									<div class="fields">
 										<div class="field half">
@@ -21,16 +21,11 @@
 								</form>
 							</section>
 							<section>
-								<h2>Follow</h2>
+								<h2>社群連結</h2>
 								<ul class="icons">
-									<li><a href="#" class="icon brands style2 fa-twitter"><span class="label">Twitter</span></a></li>
-									<li><a href="#" class="icon brands style2 fa-facebook-f"><span class="label">Facebook</span></a></li>
-									<li><a href="#" class="icon brands style2 fa-instagram"><span class="label">Instagram</span></a></li>
-									<li><a href="#" class="icon brands style2 fa-dribbble"><span class="label">Dribbble</span></a></li>
-									<li><a href="#" class="icon brands style2 fa-github"><span class="label">GitHub</span></a></li>
-									<li><a href="#" class="icon brands style2 fa-500px"><span class="label">500px</span></a></li>
-									<li><a href="#" class="icon solid style2 fa-phone"><span class="label">Phone</span></a></li>
-									<li><a href="#" class="icon solid style2 fa-envelope"><span class="label">Email</span></a></li>
+									<li><a target="_blank" href="//facebook.com/wayne1894.teach" class="icon brands style2 fa-facebook-f"><span class="label">Facebook</span></a></li>
+									<li><a target="_blank" href="https://github.com/wayne1894" class="icon brands style2 fa-github"><span class="label">GitHub</span></a></li>
+									<li><a href="mailto: mose286778@gmail.com" class="icon solid style2 fa-envelope"><span class="label">Email</span></a></li>
 								</ul>
 							</section>
 							<ul class="copyright">
