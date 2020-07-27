0df782fff0be7f9aa6650034ab3f7eb8a8537cce
wayne1894 <mose286778@gmail.com>
2020-07-28 03:39:22 +0800

    no message

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,9 +10,6 @@ export default {
   ** See https://nuxtjs.org/api/configuration-target
   */
   target: 'static',
-  router: {
-    base: '/nuxt_demo/'
-  }
   /*
   ** Headers of the page
   ** See https://nuxtjs.org/api/configuration-head
